2c83c3ad4d585ae9337207bf4b09a22652be5513
Luka Dumancic <luka.dumancic49@gmail.com>
2021-01-14 20:11:05 +0100

    Init webpack and base

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import MundusBluetoothService from "@mundus-tech/mundus-pwa-bluetooth";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const bt = new MundusBluetoothService();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function clickEvent() {
+  bt.connect();
+}
+
+document.querySelector("#connect").addEventListener("click", clickEvent);
